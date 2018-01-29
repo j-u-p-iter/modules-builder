3c8aa4625023ab3677365b379f4c87d0ea286c9e
kirill <best.portfoleo@gmail.com>
2018-01-29 14:07:40 +0500

    Extract compiler into separate module

--- packages/modules-builder/modules/builder.js
+++ packages/modules-builder/modules/builder.js
@@ -1,41 +1,7 @@
-const fs = require('fs');
-const spawn = require('child_process').spawn;
-const chalk = require('chalk');
+const compileAccordingToInfo = require('./compiler');
 
-const { forEachObjIndexed } = require('ramda');
-
-
-const exec = (command, extraEnv) =>
-  spawn(command, {
-    shell: true,
-    env: { ...process.env, ...extraEnv },
-  });
-
-const writeMessage = message => process.stdout.write(message);
-
-
-export const compileAccordingToInfo = ({
-  messages,
-  command,
-  moduleType,
-  env = 'development',
-}) => {
-  writeMessage(chalk.blue(messages.onBeforeRun));
-
-  const execCompiling = exec(command, {
-    BABEL_ENV: moduleType,
-    NODE_ENV: env,
-  });
-
-  execCompiling.stderr.on('data', error => writeMessage(error.toString()));
-
-  execCompiling
-    .on('exit', (code) => {
-      code !== 0 && writeMessage(chalk.red(messages.onError));
-      code === 0 && writeMessage(chalk.green(messages.onSuccess));
-    });
-};
-
-
-export default const createBuilder = infoToCompile =>
+const createBuilder = infoToCompile =>
   () => forEachObjIndexed(compileAccordingToInfo, infoToCompile);
+
+
+module.exports = createBuilder;
