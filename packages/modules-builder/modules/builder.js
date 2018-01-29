9ec1089ae409c6ce05dd671a1f6e531978c28a54
kirill <best.portfoleo@gmail.com>
2018-01-29 23:02:07 +0500

    Add vertical indentation

--- packages/modules-builder/modules/builder.js
+++ packages/modules-builder/modules/builder.js
@@ -1,6 +1,7 @@
 const { forEachObjIndexed } = require('ramda');
 const compileAccordingToInfo = require('./compiler');
 
+
 const createBuilder = infoToCompile =>
   () => forEachObjIndexed(compileAccordingToInfo, infoToCompile);
 
